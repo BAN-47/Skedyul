dc08a51ab7b106988ff7810bd2ef3bc52094749d
Emmanuel D. Ugalde <macky.ugalde@gmail.com>
2026-07-16 13:05:39 +0800

    rest of dean web pages goods

--- routes/deanweb.php
+++ routes/deanweb.php
@@ -6,4 +6,28 @@ Route::get('/dean/dashboard', function () {
     return view('dean.dean_dashboard');
 })->name('dean.dashboard');
 
+Route::get('/dean/faculty_workload', function() {
+    return view('dean.faculty_workload');
+})->name('dean.faculty_workload');
+
+Route::get('/dean/departments', function() {
+    return view('dean.departments');
+})->name('dean.departments');
+
+Route::get('/dean/pending_approvals', function() {
+    return view('dean.pending_approvals');
+})->name('dean.pending_approvals');
+
+Route::get('/dean/schedule_reports', function() {
+    return view('dean.schedule_reports');
+})->name('dean.schedule_reports');
+
+Route::get('/dean/faculty_deployment', function() {
+    return view('dean.faculty_deployment');
+})->name('dean.faculty_deployment');
+
+Route::get('/dean/faculty_deployment', function() {
+    return view('dean.settings');
+})->name('dean.settings');
+
 ?>
